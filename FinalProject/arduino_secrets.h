edaae6b98242903d34d5ce5ce0f8cce35332d6d9
SungJae <88194064+SungJae01@users.noreply.github.com>
2024-06-06 00:04:56 +0900

    Update arduino_secrets.h

--- FinalProject/arduino_secrets.h
+++ FinalProject/arduino_secrets.h
@@ -1,2 +1,10 @@
-#define SECRET_SSID ""      //WiFi ID
-#define SECRET_PASS ""      //WiFi Password
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// The API key can be obtained from Firebase console > Project Overview > Project settings.
+#define API_KEY ""
+
+// User Email and password that already registerd or added in your project.
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
+#define DATABASE_URL ""
